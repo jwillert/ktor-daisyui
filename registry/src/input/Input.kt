e8e140d9688d304270c6c8e62327b673372d00fd
jwillert <d14110268+jwillert@users.noreply.github.com>
2026-05-12 17:56:41 +0200

    feat: add koInput component and slot metadata to registry

---
+++ registry/src/input/Input.kt
@@ -0,0 +1,38 @@
+package dev.jwillert.daisyui.components
+
+import kotlinx.html.FlowContent
+import kotlinx.html.InputType
+import kotlinx.html.input
+
+fun FlowContent.koInput(
+    name: String,
+    type: String = "text",
+    placeholder: String = "",
+    required: Boolean = false,
+    disabled: Boolean = false,
+    additionalClasses: String = "",
+) {
+    val inputType = when (type) {
+        "email" -> InputType.email
+        "password" -> InputType.password
+        "number" -> InputType.number
+        "tel" -> InputType.tel
+        "url" -> InputType.url
+        "search" -> InputType.search
+        "date" -> InputType.date
+        "hidden" -> InputType.hidden
+        "checkbox" -> InputType.checkBox
+        "radio" -> InputType.radio
+        else -> InputType.text
+    }
+    val cssClasses = listOfNotNull(
+        "input input-bordered",
+        additionalClasses.takeIf { it.isNotEmpty() }
+    ).joinToString(" ")
+
+    input(type = inputType, name = name, classes = cssClasses) {
+        if (placeholder.isNotEmpty()) this.placeholder = placeholder
+        if (required) attributes["required"] = "required"
+        if (disabled) attributes["disabled"] = "disabled"
+    }
+}
